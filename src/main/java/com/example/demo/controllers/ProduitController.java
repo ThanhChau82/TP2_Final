f311589e1de9d2a8cc9b43676e66685240cf9242
ThanhChau82 <thanh.dalencon@gmail.com>
2021-06-02 09:02:48 +0200

    Changement Validation (suite)

--- src/main/java/com/example/demo/controllers/ProduitController.java
+++ src/main/java/com/example/demo/controllers/ProduitController.java
@@ -13,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.daos.ProduitRepository;
 import com.example.demo.entites.Produit;
+import com.example.demo.validations.ValidProduit;
 
 @RestController
 @RequestMapping("produit")
 public class ProduitController {
 	@Autowired
 	private ProduitRepository produitRepository; 
+	
 	
 	/**
 	 * Ajouter un produit dans la bdd
@@ -27,7 +29,13 @@ public class ProduitController {
 	 */
 	@PostMapping
 	public Produit addProduit(@Valid @RequestBody Produit produit) { // @Valid : vérifier au préalable les validations via Annotations côtés entities (class Produit)
-		return produitRepository.save(produit);		
+		ValidProduit validProduit = new ValidProduit();
+		if (validProduit.verifTitre(produit.getTitre()) && validProduit.verifPrix(produit.getPrixU(), produit.getPrixV())
+			&& validProduit.verifQuantite(produit.getQuantite())) {
+			return produitRepository.save(produit);	
+		}
+		
+		return null;
 	}
 	
 	@GetMapping
